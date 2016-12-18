408dbfebb6f74e77fcacffe8602ce316af498c81
JesseHan <jesse.18@163.com>
2016-12-18 23:33:28 +0800

    init

--- src/main/java/com/fruit/FigApplication.java
+++ src/main/java/com/fruit/FigApplication.java
@@ -2,11 +2,15 @@ package com.fruit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.EnableZipkinServer;
 
+@EnableEurekaClient
+@EnableZipkinServer
 @SpringBootApplication
 public class FigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FigApplication.class, args);
+    }
 }
