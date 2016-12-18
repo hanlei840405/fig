773d7b6c080ca5160fb72367e2a993c4e343d1c4
JesseHan <jesse.18@163.com>
2016-12-18 22:42:54 +0800

    init

---
+++ src/main/java/com/fruit/FigApplication.java
@@ -0,0 +1,12 @@
+package com.fruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FigApplication.class, args);
+	}
+}
